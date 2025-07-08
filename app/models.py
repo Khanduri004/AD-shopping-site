5c332390dfdf2c12df0397d03994c93a92ba5efa
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 15:25:17 +0100

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    image_url = db.Column(db.String(200))
