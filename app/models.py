1373f48324bbce0b405859fcf8c635be70997e00
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 15:42:52 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from . import db
+# app/models.py
+from .extensions import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    image_url = db.Column(db.String(200))
+    # more fields here
+
